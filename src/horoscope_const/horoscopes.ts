ad0e3368eef5e65a56e63564bfb715eedeb3bcc6
Pablo Lucero <115800617+Wilson8jsn@users.noreply.github.com>
2023-12-23 21:25:11 -0500

    commit

---
+++ src/horoscope_const/horoscopes.ts
@@ -0,0 +1,14 @@
+export const horoscopeImages: { [key: string]: string } = {
+  Tauro: "/src/assets/taurus.png",
+  Aries: "/src/assets/aries.png",
+  Géminis: "/src/assets/gemini.png",
+  Cáncer: "/src/assets/cancer.png",
+  Leo: "/src/assets/leo.png",
+  Virgo: "/src/assets/virgo.png",
+  Libra: "/src/assets/libra.png",
+  Escorpio: "/src/assets/scorpio.png",
+  Sagitario: "/src/assets/sagitarius.png",
+  Capricornio: "/src/assets/capricorn.png",
+  Acuario: "/src/assets/aquarius.png",
+  Piscis: "/src/assets/piscis.png",
+};
